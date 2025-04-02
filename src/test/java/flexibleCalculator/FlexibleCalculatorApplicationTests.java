7571fd84bffa6fc7a36bd121da1f0a2745cc08b5
Grace1615 <wxu0811@outlook.com>
2025-04-02 12:37:11 -0700

    Initial commit: Setup Spring Boot project structure

---
+++ src/test/java/flexibleCalculator/FlexibleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package flexibleCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexibleCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
